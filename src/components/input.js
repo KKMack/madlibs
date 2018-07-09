04220673eab4ff24e7fb700e7103fe3a4345282a
Kaden <you@example.com>
2018-07-09 11:30:52 -0600

    I gave up

--- src/components/input.js
+++ src/components/input.js
@@ -4,7 +4,7 @@ const Input = ({title, state, name}, onChange, index) => {
     return (
         <div key={index} className="input">
             <label className="input__number">{index + 1}</label>
-            <input name={name} value={state} onChange={onChange}/>
+            <input placeholder={title} name={name} value={state} onChange={onChange}/>
             <label className="input__title">{title}</label>
         </div>
     )
